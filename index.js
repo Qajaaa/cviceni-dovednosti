faedc2b85f394cae9ceac62504b00b02c72ade40
Alena Holubova <aja.holubova@email.cz>
2024-03-03 14:20:14 +0100

    uprava

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('funguju!');
+
+
+let urovenHTML = prompt ("Vyjadrete svou uroven znalosti HTML cislem od 0 do 100")
+let urovenCSS = prompt ("Vyjadrete svou uroven znalosti CSS cislem od 0 do 100")
+let urovenJS = prompt ("Vyjadrete svou uroven znalosti Java Sriptu cislem od 0 do 100")
+
+
+
+//const soupatkoHTML = document.querySelector(".skill__progress");
+//newsElm.style.maxWidth = 'uroven';
