c7b0242d4882845744faae08dbd6c86f7cca6d8f
Alena Holubova <aja.holubova@email.cz>
2024-03-07 17:52:03 +0100

    oprava3

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
     Tato funkce při zavolání nastaví posuvník se zadaným id na správnou hodnotu. 
     Použijte tuto funkci ve vašem kódu, abyste nemuseli při nastavování posuvníků psát třikrát po sobě téměř stejný kód.
 */
-    let urovenHTML = prompt ("Vyjadrete svou uroven znalosti HTML cislem od 0 do 100");
-    let urovenCSS = prompt ("Vyjadrete svou uroven znalosti CSS cislem od 0 do 100");
-    let urovenJS = prompt ("Vyjadrete svou uroven znalosti Java Sriptu cislem od 0 do 100");
+    let urovenHTML = prompt ("Vyjádřete svou úroveň znalostí HTML čislem od 0 do 100");
+    let urovenCSS = prompt ("Vyjádřete svou úroveň znalostí CSS číslem od 0 do 100");
+    let urovenJS = prompt ("Vyjádřete svou úroveň znalostí Java Sriptu číslem od 0 do 100");
 
     const updateSkill =(id,value) => {
         document.querySelector("#" +id+" .skill__progress").style.width = value + "%" ;
@@ -20,20 +20,36 @@
     if (urovenHTML >= 0 && urovenHTML <= 100) {
         updateSkill ("skill1",urovenHTML)
     }else {
-        alert("Neplatná hodnota pro dovednost HTML.Zadejte prosím číslo v rozsahu 0-100");
+        alert("Neplatná hodnota pro dovednost HTML. Zadejte prosím číslo v rozsahu 0-100");
     }
 
     if (urovenHTML >= 0 && urovenHTML <= 100) {
         updateSkill ("skill2",urovenCSS)
     }else {
-        alert("Neplatná hodnota pro dovednost CSS.Zadejte prosím číslo v rozsahu 0-100");
+        alert("Neplatná hodnota pro dovednost CSS. Zadejte prosím číslo v rozsahu 0-100");
     }
 
 
     if (urovenHTML >= 0 && urovenHTML <= 100) {
         updateSkill ("skill3",urovenJS)
     }else {
-        alert("Neplatná hodnota pro dovednost JS.Zadejte prosím číslo v rozsahu 0-100");
+        alert("Neplatná hodnota pro dovednost JS. Zadejte prosím číslo v rozsahu 0-100");
     }
 
- 
+
+    let nejvetsiCislo = document.getElementById ("nejvetsiCislo")
+    const max3 = () => {
+        if (urovenHTML > urovenCSS && urovenHTML > urovenJS){
+
+           nejvetsiCislo.innerHTML = "Vaše úroveň dovedností v HTML je vetší než úroveň dovedností v CSS a JS." 
+        }else if (urovenCSS > urovenHTML && urovenCSS > urovenJS){
+
+            nejvetsiCislo.innerHTML = "Vaše úroveň dovedností v CSS je vetší než úroveň dovedností v HTML a JS."
+        }else if (urovenJS > urovenHTML && urovenJS > urovenCSS){
+
+            nejvetsiCislo.innerHTML = "Vaše úroveň dovedností v JS je vetší než úroveň dovedností v CSS a HTML."
+        }else{
+            nejvetsiCislo.innerHTML = "Číslo nebylo zadáno."
+        }
+    }
+max3();
